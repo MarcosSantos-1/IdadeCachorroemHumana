3b07aa28ad9a395d2455c642f84b5be9ea2ce457
Marcos Vinicius <marcotas203@gmail.com>
2024-02-16 01:57:24 -0300

    First Commit

---
+++ idadeDeCachorro/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  idadeDeCachorro
+//
+//  Created by Pc MacOS on 16/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func descobrirIdade(_ sender: Any) {
+        print("Comando Enviado!")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
